dd0f4282163f114d1f184ac809a541bf3fb6b7df
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-01-29 10:25:57 +0100

    Add Cards

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,15 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
 })
 export class GameService {
 
+    private juegos: Game[] = [];
+
     constructor(private http: HttpClient) { }
 
     getGames() {
-        return this.http.get<Game[]>(`${environment.url}/api/games`);
+        if( this.juegos.length > 0) {
+            // No tenemos juegos
+            console.log('Desde cache');
+            return of( this.juegos )
+        } else {
+            console.log('Desde el Get http');
+            return this.http.get<Game[]>(`${environment.url}/api/games`)
+                .pipe(
+                    tap(juegos => {
+                        this.juegos = juegos
+                    })
+                )
+        }
     }
 }
